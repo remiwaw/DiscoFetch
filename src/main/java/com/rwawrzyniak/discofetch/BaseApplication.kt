3c143a683042f763b09c2e90fb70048b29d1beeb
Remigiusz Wawrzyniak <remigiusz.wawrzyniak@ext.sys.aok.de>
2020-12-03 01:20:18 +0100

    Basic dependencies added

---
+++ src/main/java/com/rwawrzyniak/discofetch/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.rwawrzyniak.discofetch
+
+import android.app.Application
+
+open class BaseApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
