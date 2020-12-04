aff85d8092c3b16e80e5be37613b9923c3b81e0b
Remigiusz Wawrzyniak <remigiusz.wawrzyniak@ext.sys.aok.de>
2020-12-04 16:54:16 +0100

    Added hilt

--- src/main/java/com/rwawrzyniak/discofetch/framework/presentation/BaseApplication.kt
+++ src/main/java/com/rwawrzyniak/discofetch/framework/presentation/BaseApplication.kt
@@ -1,10 +1,9 @@
 package com.rwawrzyniak.discofetch.framework.presentation
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 open class BaseApplication : Application(){
 
-    override fun onCreate() {
-        super.onCreate()
-    }
 }
